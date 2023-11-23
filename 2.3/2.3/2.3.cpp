5b8db97895f7533852e297a6b8eb1cd757233c07
Vlad <151467742+fgolik@users.noreply.github.com>
2023-11-23 21:49:47 +0300

    2.3 solving added

---
+++ 2.3/2.3/2.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>//Даны натуральные числа n, m. Получить все общие кратные, меньшие mn.
+using namespace std;
+
+int main()
+{
+	int m, n;
+	cout << "n=" << endl;
+	cin >> n;
+	cout << "m=";
+	cin >> m;
+	for (int i = 1; i < m * n; i++)
+	{
+		if (n % i == 0 && m % i == 0)
+		{
+			cout << i << endl;
+
+		}
+	}
+}
